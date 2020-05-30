fa050d0778ac15599c5771f48fec7ce1a68c8c57
hari <anilypb@gmail.com>
2020-05-30 12:04:08 +0530

    initial commit

---
+++ src/test/java/seleniumPackage/launchBrowser.java
@@ -0,0 +1,38 @@
+package seleniumPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class launchBrowser {
+	
+	String title;
+
+	@Test
+	public void launchBrowser(){
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		title = driver.getTitle();
+		Assert.assertEquals("Google", title);
+		driver.quit();
+	}
+
+	@Test
+	public void searchItems() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		String title = driver.getTitle();
+		driver.findElement(By.name("q")).sendKeys("world");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		title = driver.getTitle();
+		Assert.assertEquals("world - Google Search", title);
+		driver.quit();
+	}
+}
